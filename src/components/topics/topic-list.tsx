4e532f0453ddd78ee67f327729d7e59a0161e9bd
Javier Amarilla <javier.amarilla@casademoneda.gob.ar>
2024-08-08 18:59:01 -0300

    create post form and comment file

--- src/components/topics/topic-list.tsx
+++ src/components/topics/topic-list.tsx
@@ -10,7 +10,7 @@ export default async function TopicList() {
     return (
       <div key={topic.id}>
         <Link href={paths.topicShow(topic.slug)}>
-          <Chip color="warning" variant="shadow">
+          <Chip color="warning" variant="bordered">
             {topic.slug}
           </Chip>
         </Link>
